fd589365ec6fd2ce7b3f1f94e47f7ab3010604b5
Daniel Spector <danielyspector@gmail.com>
2013-07-19 03:43:39 +0000

    Fix web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-fs.readFileSync(index.html);
-var buffer = new Buffer(
-buffer.toString(index.html);
+    var buffer = new Buffer();
+    response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
