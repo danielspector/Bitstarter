2bf25900db95a75d9d17105bc25765d759564a98
Daniel Spector <danielyspector@gmail.com>
2013-07-19 04:31:59 +0000

    Fix up web.js syntax

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer)express.logger());
-var HTMLImport = fs.readFileSync('index,html');
+var app = express.createServer(express.logger());
+var HTMLImport = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
    response.writeHead(200, {
