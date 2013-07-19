ecf9186033521189fbbb7b2717c1296880302699
Daniel Spector <danielyspector@gmail.com>
2013-07-19 03:34:41 +0000

    Added file sync

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+fs.readFileSync(index.html);
+var buffer = new Buffer(
+buffer.toString(index.html);
 });
 
 var port = process.env.PORT || 5000;
